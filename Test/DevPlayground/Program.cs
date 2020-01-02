899469fca667d1797adf3416a3ac3667f4da68dc
HungPV <hungpv@goquo.com>
2020-01-02 09:48:41 +0700

    add playground project

---
+++ Test/DevPlayground/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using SimpleAuth.Core.Utils;
+using SimpleAuth.Services;
+using SimpleAuth.Shared;
+
+namespace DevPlayground
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var encrypt = new DefaultEncryptionService(new EncryptionUtils.EncryptionKeyPair
+            {
+                PublicKey = Constants.Test.Rsa2048PublicKey,
+                PrivateKey = Constants.Test.Rsa2048PrivateKey
+            }).Encrypt("LoremIpSum");
+            Console.WriteLine(encrypt);
+        }
+    }
+}
