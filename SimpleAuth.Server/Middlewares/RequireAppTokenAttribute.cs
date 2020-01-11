f106e78099138e98a7a55a4fbd0c48747bb28966
HungPV <hungpv@goquo.com>
2020-01-11 17:04:43 +0700

    'update'

--- SimpleAuth.Server/Middlewares/RequireAppTokenAttribute.cs
+++ SimpleAuth.Server/Middlewares/RequireAppTokenAttribute.cs
@@ -50,7 +50,7 @@ namespace SimpleAuth.Server.Middlewares
                     {
                         actionExecutingContext.Result =
                             StatusCodes.Status426UpgradeRequired.WithMessage(
-                                $"Mis-match token {nameof(TokenInfo.Version)}, expected {obj.Version}, current {currentTokenVersion}");
+                                $"Mis-match token {nameof(TokenInfo.Version)}, expected {currentTokenVersion} but {obj.Version}");
                         return;
                     }
                         
