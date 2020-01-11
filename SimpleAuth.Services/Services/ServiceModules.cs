486d8c6ecb4051c092c6e1ca4cddf10d7bfe1959
HungPV <hungpv@goquo.com>
2020-01-11 16:33:20 +0700

    Cache service must be Singleton

--- SimpleAuth.Services/Services/ServiceModules.cs
+++ SimpleAuth.Services/Services/ServiceModules.cs
@@ -11,7 +11,7 @@ namespace SimpleAuth.Services
             serviceCollection.AddTransient<IRoleService, DefaultRoleService>();
             serviceCollection.AddTransient<IRoleGroupService, DefaultRoleGroupService>();
             serviceCollection.AddTransient<IUserService, DefaultUserService>();
-            serviceCollection.AddTransient<ITokenInfoService, DefaultTokenInfoService>();
+            serviceCollection.AddSingleton<ITokenInfoService, DefaultTokenInfoService>();
         }
     }
 }
