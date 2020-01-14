56c7c90b8eec3cb609b39165384586c3214b9016
HungPV <hungpv@goquo.com>
2020-01-14 10:59:47 +0700

    'update'

--- SimpleAuth.Server/Program.cs
+++ SimpleAuth.Server/Program.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -7,21 +9,57 @@ namespace SimpleAuthServer
 {
     internal static class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFiles()
+                .AddEnvironmentVariables()
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .Enrich.FromLogContext()
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Getting the motors running...");
+
+                CreateHostBuilder(args).Build().Run();
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .UseSerilog()
                 .ConfigureAppConfiguration((hostBuilder, builder) =>
                     {
-                        builder.AddJsonFile("appsettings.json", optional: true);
-                        builder.AddJsonFile("/configmaps/simple-auth/appsettings.json", optional: true);
+                        builder.AddJsonFiles();
                         builder.AddEnvironmentVariables();
                     }
                 )
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
+                .UseSerilog();
+    }
+
+    internal static class ConfigurationExtensions
+    {
+        public static IConfigurationBuilder AddJsonFiles(this IConfigurationBuilder builder)
+        {
+            builder.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            builder.AddJsonFile("/configmaps/simple-auth/appsettings.json", optional: true, reloadOnChange: true);
+            return builder;
+        }
     }
 }
