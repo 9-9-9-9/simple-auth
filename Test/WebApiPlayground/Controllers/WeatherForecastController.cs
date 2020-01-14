9099ab41a1fba3166676e863d1d5fa8ad8ef32d8
HungPV <hungpv@goquo.com>
2020-01-14 13:43:07 +0700

    update WeatherForecasrController

--- Test/WebApiPlayground/Controllers/WeatherForecastController.cs
+++ Test/WebApiPlayground/Controllers/WeatherForecastController.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using SimpleAuth.Client.AspNetCore.Attributes;
-using SimpleAuth.Client.AspNetCore.Middlewares;
 using SimpleAuth.Shared.Enums;
 
 namespace WebApiPlayground.Controllers
@@ -17,62 +15,39 @@ namespace WebApiPlayground.Controllers
         private static readonly string[] Summaries = {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        
+        [HttpGet("0")]
+        public IEnumerable<WeatherForecast> GetWithoutPermission()
         {
-            _logger = logger;
+            return GetSample();
         }
 
         [HttpGet]
         [SaPermission(Permission.View, "a")]
         [SaPermission(Permission.View, "b")]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<WeatherForecast> GetWithControllerModule()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-                {
-                    Date = DateTime.Now.AddDays(index),
-                    TemperatureC = rng.Next(-20, 55),
-                    Summary = Summaries[rng.Next(Summaries.Length)]
-                })
-                .ToArray();
+            return GetSample();
         }
         
         [HttpGet("2")]
         [SaModule("best")] // Override attribute of class
         [SaPermission(Permission.View)]
-        public IEnumerable<WeatherForecast> Get2()
+        public IEnumerable<WeatherForecast> GetWithOverridenModule()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-                {
-                    Date = DateTime.Now.AddDays(index),
-                    TemperatureC = rng.Next(-20, 55),
-                    Summary = Summaries[rng.Next(Summaries.Length)]
-                })
-                .ToArray();
+            return GetSample();
         }
         
         [HttpGet("3")]
-        public IEnumerable<WeatherForecast> Get3()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-                {
-                    Date = DateTime.Now.AddDays(index),
-                    TemperatureC = rng.Next(-20, 55),
-                    Summary = Summaries[rng.Next(Summaries.Length)]
-                })
-                .ToArray();
-        }
-        
-        [HttpGet("4")]
         [SaModule("best")] // Override attribute of class
         [SaPermission(Permission.View, "a")]
         [SaPermission(Permission.View, "b")]
-        public IEnumerable<WeatherForecast> Get4()
+        public IEnumerable<WeatherForecast> GetWithMissingPermission()
+        {
+            return GetSample();
+        }
+        
+        private IEnumerable<WeatherForecast> GetSample()
         {
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
