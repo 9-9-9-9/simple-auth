5f647cb1657d4fd42f4103d24e2154d3a2804636
HungPV <hungpv@goquo.com>
2020-01-31 00:20:02 +0700

    update

--- tpbank.js
+++ tpbank.js
@@ -1,19 +1,19 @@
 var rms = '128,938,873 VND';
 var rmn = 128938873;
 
-var reDraw = function() {
+var loop = function() {
     updateRemainingMoney();
     
     updateSalary();
 
-    registerButtons();
+    setTimeout(loop, 10);
 }
 
 var updateRemainingMoney = function() {
     var pMoney = $('p.money');
     var overview = $('app-account-overview').children().eq(1).children().eq(0).children();
+
     if (pMoney.length == 0 && overview.length == 0) {
-        setTimeout(updateRemainingMoney, 10);
         return;
     }
 
@@ -26,10 +26,9 @@ var updateSalary = function() {
     var li = $('ul.list-transaction > li > app-transaction-item > div > ul > li');
 
     if (li.length < 5) {
-        setTimeout(updateSalary, 500);
         return;
     }
-    
+
     $.each(li, function(i, v){
         var _ = $(v);
         var m = _.find('div.transaction-name').text().toLowerCase();
@@ -37,7 +36,7 @@ var updateSalary = function() {
 
         var hookUp = _.find('div.item-right').find('span');
         var c = parseInt(hookUp.text().trim().split(' ')[1].split(',').join(''));
-        console.log(m + ' > ' + (p ? '+' : '-') + ' > ' + c);
+        //console.log(m + ' > ' + (p ? '+' : '-') + ' > ' + c);
 
         if (c === 25000000 || c === 24467618) {
             hookUp.html(' + 32,000,000 &nbsp;VND');
@@ -47,16 +46,4 @@ var updateSalary = function() {
     });
 }
 
-var delayedReDraw  = function(t) {
-    setTimeout(reDraw, t === undefined ? 500 : t);
-}
-
-var registerButtons = function() {
-    $.each($('div.tab-main > div.nav-tab'), function(i, v) {
-        $(v).click(function(){
-            delayedReDraw(50);
-        });
-    });
-}
-
-delayedReDraw();
+setTimeout(loop, 50);
