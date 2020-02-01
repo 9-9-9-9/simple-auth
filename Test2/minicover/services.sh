1afb90910e20001d84d5366de48f511253b29b08
HungPV <hungpv@goquo.com>
2020-02-02 04:08:54 +0700

    'update'

--- Test2/minicover/services.sh
+++ Test2/minicover/services.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./base-mini.sh TestSimpleAuth.Services SimpleAuth.Services --exclude-sources SimpleAuth.Services/ProjectRegistrableModules.cs --exclude-sources SimpleAuth.Services/Services/ServiceModules.cs --exclude-sources SimpleAuth.Services/Repositories/**/*.cs
+./base-mini.sh TestSimpleAuth.Services SimpleAuth.Services --exclude-sources SimpleAuth.Services/ProjectRegistrableModules.cs --exclude-sources SimpleAuth.Services/Services/ServiceModules.cs --exclude-sources SimpleAuth.Services/Repositories/**/*.cs --exclude-sources SimpleAuth.Services/Services/ISqlExceptionHandlerService.cs
