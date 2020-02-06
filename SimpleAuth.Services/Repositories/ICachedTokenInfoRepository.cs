b3c3c4e496077f7cb4fd9a71b56945b05254297b
HungPV <hungpv@goquo.com>
2020-02-07 00:07:10 +0700

    fixed implementation of Memory cached repo

--- SimpleAuth.Services/Repositories/ICachedTokenInfoRepository.cs
+++ SimpleAuth.Services/Repositories/ICachedTokenInfoRepository.cs
@@ -3,26 +3,39 @@ using SimpleAuth.Services.Entities;
 
 namespace SimpleAuth.Repositories
 {
-    public interface ICachedTokenInfoRepository : ICachedRepository<TokenInfo>
+    public interface ICachedTokenInfoRepository
     {
         void Push(TokenInfo tokenInfo);
         TokenInfo Get(string corp, string app);
+        void Clear(string corp, string app);
     }
 
-    public class CachedTokenInfoRepository : MemoryCachedRepository<TokenInfo>, ICachedTokenInfoRepository
+    public class CachedTokenInfoRepository : ICachedTokenInfoRepository
     {
+        private readonly ICachedRepository<TokenInfo> _memoryCachedRepository;
+
+        public CachedTokenInfoRepository(ICachedRepository<TokenInfo> memoryCachedRepository)
+        {
+            _memoryCachedRepository = memoryCachedRepository;
+        }
+
         public void Push(TokenInfo tokenInfo)
         {
-            Push(tokenInfo, BuildKey(tokenInfo), tokenInfo.Corp, tokenInfo.App);
+            _memoryCachedRepository.Push(tokenInfo, BuildKey(tokenInfo), tokenInfo.Corp, tokenInfo.App);
         }
 
         public TokenInfo Get(string corp, string app)
         {
-            return Get(BuildKey(new TokenInfo
+            return _memoryCachedRepository.Get(BuildKey(new TokenInfo
             {
                 Corp = corp,
                 App = app
             }), corp, app);
+        }
+
+        public void Clear(string corp, string app)
+        {
+            _memoryCachedRepository.Clear(corp, app);
         }
 
         private string BuildKey(TokenInfo tokenInfo) => $"{tokenInfo.Corp}@{tokenInfo.App.Or(string.Empty)}";
