88bc6079c8c728a528c5a712750757904581fd6f
HungPV <hungpv@goquo.com>
2020-02-18 21:00:05 +0700

    'update'

--- ManagementTools/CorpManagementConsole/Commands/GenerateAppPermissionTokenCommand.cs
+++ ManagementTools/CorpManagementConsole/Commands/GenerateAppPermissionTokenCommand.cs
@@ -16,19 +16,23 @@ namespace CorpManagementConsole.Commands
 
         protected override Task DoMainJob(string[] args)
         {
-            return Print(_administrationService.GenerateAppPermissionTokenAsync(args[0]));
+            return Print(_administrationService.GenerateAppPermissionTokenAsync(args[0], ParseBoolean(args[1]) ?? true));
         }
 
         public override string[] GetParametersName()
         {
-            return new[] {"App"};
+            return new[] {"App", "Public (y/n)"};
         }
 
         protected override IEnumerable<string> GetOthersArgumentsProblems(params string[] args)
         {
-            yield break;
+            var @public = args[1];
+            if (IsCorrectBool(@public))
+                yield break;
+            else
+                yield return "Invalid @public, accepted y/n";
         }
 
-        protected override int[] IdxParametersCanNotBeBlank => new[] {0};
+        protected override int[] IdxParametersCanNotBeBlank => new[] {0, 1};
     }
 }
