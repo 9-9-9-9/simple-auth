246e64faa11a683e5423ed795aeee220362ddafb
HungPV <hungpv@goquo.com>
2020-02-21 16:14:57 +0700

    'update'

--- Tests/Examples/WebApiPlayground/Program.cs
+++ Tests/Examples/WebApiPlayground/Program.cs
@@ -10,18 +10,12 @@ namespace WebApiPlayground
     {
         public static void Main(string[] args)
         {
-            var scanner = new PermissionScanner<SaModuleAttribute, SaPermissionAttribute>();
-            scanner
-                .AddAssembly(typeof(Program).Assembly)
-                .ScanToFile();
-
-
-            var scanner2 = new PermissionGenerator<SaModuleAttribute, SaPermissionAttribute>(
+            var scanner = new PermissionGenerator<SaModuleAttribute, SaPermissionAttribute>(
                 "test",
                 "wap",
                 "staging", "prod"
             );
-            scanner2
+            scanner
                 .AddAssembly(typeof(Program).Assembly)
                 .ScanToFile();
 
